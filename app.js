7f1eeeef8a1ebf15d7a24cafb5a13340031f007d
Alexander <slepnev-alexander@mail.ru>
2018-02-15 21:41:59 +0300

    MongoDb
    Mongoose

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 
 const productRoutes = require("./api/routes/products");
 const orderRoutes = require("./api/routes/orders");
+
+mongoose.connect('mongodb://localhost:27017/sports-store', {});
+
+mongoose.connection.on("open", function(ref) {
+  console.log("Connected to mongo server.");
+});
+
+mongoose.connection.on("error", function(err) {
+  console.log("Could not connect to mongo server!");
+});
 
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+// Headers
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
@@ -20,9 +32,11 @@ app.use((req, res, next) => {
   next();
 });
 
+// Routes
 app.use("/products", productRoutes);
 app.use("/orders", orderRoutes);
 
+// Errors
 app.use((req, res, next) => {
   const error = new Error("Not found");
   error.status = 404;
